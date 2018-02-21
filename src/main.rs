1335c6e8989511d14087fb86d39642512b7ef0cc
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-02-21 11:55:11 +0100

    Add argparse to remove branch from log

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+extern crate argparse;
 extern crate colored;
 
+use argparse::{ArgumentParser, Store};
+use colored::*;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Write};
 use std::collections::HashMap;
 use std::env;
 use std::process;
-use colored::*;
+
+struct Configuration {
+    delete : String
+}
+
+fn set_argparse() -> Configuration{
+
+    let mut configuration = Configuration{delete: "".to_string()};
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("will use a crontab & Git to log wich current branch you work on..");
+        ap.refer(&mut configuration.delete)
+            .add_option(&["-d", "--delete"], Store,
+            "Delete given branch");
+        ap.parse_args_or_exit();
+    }
+
+    return configuration;
+}
 
 /// Open file & get Line to iterate on it
 fn get_logs() -> std::io::Lines<BufReader<File>> {
@@ -29,6 +50,30 @@ fn get_logs() -> std::io::Lines<BufReader<File>> {
 
 
 fn main() {
+    let configuration = set_argparse();
+
+    // delete branch if asked
+    if !configuration.delete.is_empty() {
+        // store logs contained in folder
+        let mut logs : Vec<String> = Vec::new();
+        for log in get_logs() {
+            let branch: String = log.unwrap();
+            // filter log to remove specified branch
+            if branch != configuration.delete {
+                logs.push(branch);
+            }
+        }
+        // open file in write mode (clean file)
+        let mut file = match File::create(".tickets_count.log") {
+            Ok(file) => file,
+            Err(_) => panic!("Can't open '.tickets_count.log' file, check access rights"),
+        };
+
+        for log in logs {
+            let _ = file.write_all(format!("{}\r\n", log).as_bytes());
+        }
+    }
+
     let mut counts_branchs: HashMap<String, u64> = HashMap::new();
     let mut max_space: usize = 0;
 
